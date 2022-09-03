a5ffed4f9f44cf45e965e25adfa15caf81812d91
Denis S <makafonov@gmail.com>
2022-09-03 20:57:14 +0500

    Добавлены схемы и модели movie, user

---
+++ src/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: isEmail,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
