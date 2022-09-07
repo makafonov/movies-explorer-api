4449588cc3131e7de5951f32399b3f08d62afbe3
Denis S <makafonov@gmail.com>
2022-09-07 12:46:52 +0500

    Фикс модели Movie, функция поиска пользоваля по кредам

--- src/models/movie.js
+++ src/models/movie.js
@@ -43,7 +43,7 @@ const movieSchema = new mongoose.Schema({
     required: true,
   },
   movieId: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Number,
     required: true,
   },
   nameRU: {
