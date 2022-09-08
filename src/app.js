d7001627c4fdf055bb47efeb8be61141272a0493
Denis S <makafonov@gmail.com>
2022-09-08 12:15:28 +0500

    Подключение логгера перед лимитером

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
+app.use(requestLogger);
 app.use(limiter);
 app.use(express.json());
-app.use(requestLogger);
 app.use(cors());
 app.use(router);
 app.use(errorLogger);
