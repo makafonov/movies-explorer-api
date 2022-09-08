d11f3d7a1490a3cf21cd73bcd93c8fee66997104
Denis S <makafonov@gmail.com>
2022-09-08 14:09:40 +0500

    Наименование БД берется из переменных окружения

--- src/config.js
+++ src/config.js
@@ -2,13 +2,16 @@ const rateLimit = require('express-rate-limit');
 
 let PORT;
 let SECRET_KEY;
+let DB_NAME;
 
 if (process.env.NODE_ENV === 'production') {
   PORT = process.env.PORT;
   SECRET_KEY = process.env.SECRET_KEY;
+  DB_NAME = process.env.DB_NAME;
 } else {
   PORT = 3000;
   SECRET_KEY = '5up3r-57r0ng-53cr37';
+  DB_NAME = 'moviesdb';
 }
 
 const limiter = rateLimit({
@@ -16,7 +19,7 @@ const limiter = rateLimit({
   max: 100, // limit each IP to 100 requests per windowMs
 });
 
-const DATABASE_URL = 'mongodb://localhost:27017/moviesdb';
+const DATABASE_URL = `mongodb://localhost:27017/${DB_NAME}`;
 const SALT_LENGTH = 10;
 const JWT_EXPIRES_IN = '7d';
 
